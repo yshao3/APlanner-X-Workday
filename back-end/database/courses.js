a4a6d2cb6e044e633a69d2c0154486550ccce670
MengyaoLyu <alremy@outlook.com>
2018-03-08 14:45:22 -0500

    add course collection

---
+++ back-end/database/courses.js
@@ -0,0 +1,49 @@
+db.createCollection("courses", {
+   validator: {
+      $jsonSchema: {
+         bsonType: "object",
+         required: [ "subject", "number"],
+         properties: {
+            subject: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            number: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+			title: {
+               bsonType: "string",
+               description: "must be a string and is not required"
+            },
+			prerequisite: {
+               bsonType: "arrays",
+               description: "must be a arrays and is not required"
+            },
+			corequisite: {
+               bsonType: "arrays",
+               description: "must be a arrays and is not required"
+            },
+			tags: {
+               bsonType: "arrays",
+               description: "must be a arrays and is not required"
+            },
+            when_offer: {
+			   bsonType: "arrays",
+               //enum: [ "Fall", "Spring", "Summer", "winter", "Fall and Spring"],
+               description: "arrays of enum and is not required"
+			   //"can only be one of the enum values and is required"
+            },
+			min_unit: {
+				bsonType: "int",
+				minimum: 0,
+				description: "must be an integer and is not required"
+			}
+			description: {
+				bsonType: "string",
+                description: "must be a string and is not required"
+			}
+         }
+      }
+   }
+})
