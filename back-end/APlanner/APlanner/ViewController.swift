0c4514c360c5abcaa188f65d6167959fc9260410
MengyaoLyu <alremy@outlook.com>
2018-04-09 16:49:41 -0400

    finish two UI views

--- back-end/APlanner/APlanner/ViewController.swift
+++ back-end/APlanner/APlanner/ViewController.swift
@@ -2,22 +2,34 @@
 //  ViewController.swift
 //  APlanner
 //
-//  Created by MLyu on 21/03/2018.
+//  Created by MLyu on 27/03/2018.
 //  Copyright © 2018 Team-I. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    //MARK: Properties
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+
+        // Do any additional setup after loading the view.
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
